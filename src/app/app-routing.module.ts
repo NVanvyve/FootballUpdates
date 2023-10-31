92641ea59cf73f0e8dfc4b5282cd1ad931e868fb
Nicolas Vanvyve <nicolas.vanvyve@outlook.be>
2023-10-31 15:45:53 +0100

    Title and background

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {ExtraOptions, RouterModule, Routes} from '@angular/router';
+
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  enableTracing: false,
+};
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'football-updates';
+  title = 'Football Updates';
 }
