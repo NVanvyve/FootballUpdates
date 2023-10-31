efc3bec335cbc707d735afafe9ca51b97fd25bc2
nvanvyve <nicolas.vanvyve@outlook.be>
2023-10-31 19:59:30 +0100

    Routing based on service value

--- src/app/components/guards/league.guard.ts
+++ src/app/components/guards/league.guard.ts
@@ -4,7 +4,6 @@ import {inject} from "@angular/core";
 
 export const leagueGuard: CanActivateFn = (route, _state): boolean | UrlTree => {
   const param: string = String(route.params['country']);
-  console.log('param', param);
   const isValid: boolean = inject(CountryService).isAValidCountry(param);
   return isValid ? true : inject(Router).createUrlTree(['/not-found']);
 };
