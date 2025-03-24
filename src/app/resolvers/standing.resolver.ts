9d1367e7ba88adf41e2ff4cb7de2ebd092676d06
Nicolas <nicolas.vanvyve@outlook.be>
2025-03-24 09:48:10 +0100

    Handle API errors in resolvers by navigating to error page

--- src/app/resolvers/standing.resolver.ts
+++ src/app/resolvers/standing.resolver.ts
@@ -1,13 +1,22 @@
-import {ResolveFn} from '@angular/router';
+import {ResolveFn, Router} from '@angular/router';
 import {StandingsTableElement} from "../model/standings.model";
 import {inject} from "@angular/core";
 import {CountryService} from "../services/country.service";
 import {FootballCountry} from "../model/football-country.model";
 import {StandingsApiService} from "../services/standings-api.service";
+import {catchError, of} from "rxjs";
 
 export const standingResolver: ResolveFn<StandingsTableElement[]> = (route, _state) => {
+  const router = inject(Router);
+
   const country: string = route.paramMap.get('country')! // Present and valid => Guaranteed by the guard
   const footballCountry: FootballCountry = inject(CountryService).getCountry(country);
-  return inject(StandingsApiService).getStandingsByCountry(footballCountry.name)
+  let standingsApiService = inject(StandingsApiService);
+  return standingsApiService.getStandingsByCountry(footballCountry.name).pipe(
+    catchError(_ => {
+      router.navigate(['/error']);
+      return of([]);
+    })
+  )
 }
 
--- src/app/resolvers/team-details.resolver.ts
+++ src/app/resolvers/team-details.resolver.ts
@@ -1,11 +1,14 @@
-import {ResolveFn} from '@angular/router';
+import {ResolveFn, Router} from '@angular/router';
 import {TeamDetails} from "../model/fixtures.model";
 import {inject} from "@angular/core";
 import {CountryService} from "../services/country.service";
 import {FootballCountry} from "../model/football-country.model";
 import {FixturesApiService} from "../services/fixtures-api.service";
+import {catchError, of} from "rxjs";
 
 export const teamDetailsResolver: ResolveFn<TeamDetails> = (route, _state) => {
+  const router = inject(Router);
+
   const country: string = route.paramMap.get('country')! // Present and valid => Guaranteed by the guard
   const leagueId: number = inject(CountryService).getCountry(country).leagueId;
   const teamId: string = route.paramMap.get('teamId')!; // Present and valid => Guaranteed by the guard
@@ -13,7 +16,11 @@ export const teamDetailsResolver: ResolveFn<TeamDetails> = (route, _state) => {
     leagueId,
     teamId: Number.parseInt(teamId)
   }
-  return inject(FixturesApiService).getFixturesTeamId(params.teamId, params.leagueId)
+  return inject(FixturesApiService).getFixturesTeamId(params.teamId, params.leagueId).pipe(
+    catchError(_ => {
+      router.navigate(['/error'])
+      return of();
+    }))
 };
 
 interface RouteParams {
