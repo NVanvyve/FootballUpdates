20aa391e345a248159fae2a29f0991977e463233
Nicolas <nicolas.vanvyve@outlook.be>
2025-03-24 18:09:54 +0100

    Refactor loader to use NgFor and update styles/animations

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'foot-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.sass'],
+  imports: [
+    NgForOf
+  ],
   standalone: true
 })
 export class LoaderComponent {
+  protected readonly Array = Array;
 }
